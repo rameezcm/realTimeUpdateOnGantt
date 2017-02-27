c90f2a1ca7ed932b6eb8968f8edc377f0c0f41d6
1206385 <1206385@01HW912912.India.TCS.com>
2017-02-27 16:30:28 +0530

    Code Change for Popup

--- src/main/java/org/atmosphere/samples/gantt/Gantt.java
+++ src/main/java/org/atmosphere/samples/gantt/Gantt.java
@@ -1,5 +1,7 @@
 package org.atmosphere.samples.gantt;
 
+import java.io.IOException;
+
 import org.atmosphere.config.service.Disconnect;
 import org.atmosphere.config.service.ManagedService;
 import org.atmosphere.config.service.Message;
@@ -9,31 +11,15 @@ import org.atmosphere.runtime.AtmosphereResourceEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
-/**
- * Simple annotated class that demonstrate the power of Atmosphere. This class supports all transports, support
- * message length guarantee, heart beat, message cache thanks to the @ManagedService.
- */
 @ManagedService(path = "/gantt")
 public final class Gantt{
     private static final Logger logger = LoggerFactory.getLogger(Gantt.class);
 
-    /**
-     * Invoked when the connection as been fully established and suspended, e.g ready for receiving messages.
-     *
-     * @param r the atmosphere resource
-     */
     @Ready
     public final void onReady(final AtmosphereResource r){
         logger.info("Browser {} connected.", r.uuid());
     }
 
-    /**
-     * Invoked when the client disconnect or when an unexpected closing of the underlying connection happens.
-     *
-     * @param event the event
-     */
     @Disconnect
     public final void onDisconnect(final AtmosphereResourceEvent event){
         if(event.isCancelled())
@@ -42,14 +28,6 @@ public final class Gantt{
             logger.info("Browser {} closed the connection", event.getResource().uuid());
     }
 
-    /**
-     * Simple annotated class that demonstrate how {@link org.atmosphere.config.managed.Encoder} and {@link org.atmosphere.config.managed.Decoder
-     * can be used.
-     *
-     * @param message an instance of {@link ChatMessage }
-     * @return the chat message
-     * @throws IOException
-     */
     @Message()
     public final Object onMessage(final Object obj) throws IOException{
         
--- src/main/webapp/javascript/application.js
+++ src/main/webapp/javascript/application.js
@@ -1,10 +1,10 @@
-angular.module('angular.atmosphere.chat', ['angular.atmosphere', 'gantt', 'gantt.table', 'gantt.movable']).controller('angularGanttCtrl', ChatController);
+angular.module('ganttUpdate', ['angular.atmosphere', 'gantt', 'gantt.table', 'gantt.movable','cgNotify']).controller('angularGanttCtrl', ChatController);
 
 
-function ChatController($scope, atmosphereService){
+function ChatController($scope, atmosphereService ,notify){
 
   
-  $scope.data = [{
+ /* $scope.data = [{
       name: 'Finalize concept',
       tasks: [{
         name: 'Sprint1',
@@ -12,7 +12,86 @@ function ChatController($scope, atmosphereService){
         to: new Date(2016, 0, 3).valueOf(),
         color: '#6ba3ff'
       }]
-    }];
+    }];*/
+  
+  $scope.data = [
+      // Order is optional. If not specified it will be assigned automatically
+      {name: 'Milestones', height: '3em', sortable: false, drawTask: false, classes: 'gantt-row-milestone', color: '#45607D', tasks: [
+          // Dates can be specified as string, timestamp or javascript date object. The data attribute can be used to attach a custom object
+          {name: 'Kickoff', color: '#93C47D', from: '2013-10-07T09:00:00', to: '2013-10-07T10:00:00', data: 'Can contain any custom data or object'},
+          {name: 'Concept approval', color: '#93C47D', from: new Date(2013, 9, 18, 18, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0), est: new Date(2013, 9, 16, 7, 0, 0), lct: new Date(2013, 9, 19, 0, 0, 0)},
+          {name: 'Development finished', color: '#93C47D', from: new Date(2013, 10, 15, 18, 0, 0), to: new Date(2013, 10, 15, 18, 0, 0)},
+          {name: 'Shop is running', color: '#93C47D', from: new Date(2013, 10, 22, 12, 0, 0), to: new Date(2013, 10, 22, 12, 0, 0)},
+          {name: 'Go-live', color: '#93C47D', from: new Date(2013, 10, 29, 16, 0, 0), to: new Date(2013, 10, 29, 16, 0, 0)}
+      ], data: 'Can contain any custom data or object'},
+      {name: 'Status meetings', tasks: [
+          {name: 'Demo #1', color: '#9FC5F8', from: new Date(2013, 9, 25, 15, 0, 0), to: new Date(2013, 9, 25, 18, 30, 0)},
+          {name: 'Demo #2', color: '#9FC5F8', from: new Date(2013, 10, 1, 15, 0, 0), to: new Date(2013, 10, 1, 18, 0, 0)},
+          {name: 'Demo #3', color: '#9FC5F8', from: new Date(2013, 10, 8, 15, 0, 0), to: new Date(2013, 10, 8, 18, 0, 0)},
+          {name: 'Demo #4', color: '#9FC5F8', from: new Date(2013, 10, 15, 15, 0, 0), to: new Date(2013, 10, 15, 18, 0, 0)},
+          {name: 'Demo #5', color: '#9FC5F8', from: new Date(2013, 10, 24, 9, 0, 0), to: new Date(2013, 10, 24, 10, 0, 0)}
+      ]},
+      {name: 'Kickoff', movable: {allowResizing: false}, tasks: [
+          {name: 'Day 1', color: '#9FC5F8', from: new Date(2013, 9, 7, 9, 0, 0), to: new Date(2013, 9, 7, 17, 0, 0),
+              progress: {percent: 100, color: '#3C8CF8'}, movable: false},
+          {name: 'Day 2', color: '#9FC5F8', from: new Date(2013, 9, 8, 9, 0, 0), to: new Date(2013, 9, 8, 17, 0, 0),
+              progress: {percent: 100, color: '#3C8CF8'}},
+          {name: 'Day 3', color: '#9FC5F8', from: new Date(2013, 9, 9, 8, 30, 0), to: new Date(2013, 9, 9, 12, 0, 0),
+              progress: {percent: 100, color: '#3C8CF8'}}
+      ]},
+      {name: 'Create concept', tasks: [
+          {name: 'Create concept', priority: 20, content: '<i class="fa fa-cog" ng-click="scope.handleTaskIconClick(task.model)"></i> {{task.model.name}}', color: '#F1C232', from: new Date(2013, 9, 10, 8, 0, 0), to: new Date(2013, 9, 16, 18, 0, 0), est: new Date(2013, 9, 8, 8, 0, 0), lct: new Date(2013, 9, 18, 20, 0, 0),
+              progress: 100, sections: {
+                  items: [
+                      {name: 'Section #1', classes:['section-1'], from: new Date(2013, 9, 10, 8, 0, 0), to: new Date(2013, 9, 13, 8, 0, 0)},
+                      {name: 'Section #2', classes:['section-2'], from: new Date(2013, 9, 13, 8, 0, 0), to: new Date(2013, 9, 15, 8, 0, 0)},
+                      {name: 'Section #3', classes:['section-3'], from: new Date(2013, 9, 15, 8, 0, 0), to: new Date(2013, 9, 16, 18, 0, 0)}
+                  ]
+              }
+          }
+      ]},
+      {name: 'Finalize concept', tasks: [
+          {id: 'Finalize concept', name: 'Finalize concept', priority: 10, color: '#F1C232', from: new Date(2013, 9, 17, 8, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0),
+              progress: 100}
+      ]},
+      {name: 'Development', children: ['Sprint 1', 'Sprint 2', 'Sprint 3', 'Sprint 4'], content: '<i class="fa fa-file-code-o" ng-click="scope.handleRowIconClick(row.model)"></i> {{row.model.name}}'},
+      {name: 'Sprint 1', tooltips: false, tasks: [
+          {id: 'Product list view', name: 'Product list view', color: '#F1C232', from: new Date(2013, 9, 21, 8, 0, 0), to: new Date(2013, 9, 25, 15, 0, 0),
+              progress: 25, dependencies: [{to: 'Order basket'}, {from: 'Finalize concept'}]}
+      ]},
+      {name: 'Sprint 2', tasks: [
+          {id: 'Order basket', name: 'Order basket', color: '#F1C232', from: new Date(2013, 9, 28, 8, 0, 0), to: new Date(2013, 10, 1, 15, 0, 0),
+              dependencies: {to: 'Checkout'}}
+      ]},
+      {name: 'Sprint 3', tasks: [
+          {id: 'Checkout', name: 'Checkout', color: '#F1C232', from: new Date(2013, 10, 4, 8, 0, 0), to: new Date(2013, 10, 8, 15, 0, 0),
+              dependencies: {to: 'Login & Signup & Admin Views'}}
+      ]},
+      {name: 'Sprint 4', tasks: [
+          {id: 'Login & Signup & Admin Views', name: 'Login & Signup & Admin Views', color: '#F1C232', from: new Date(2013, 10, 11, 8, 0, 0), to: new Date(2013, 10, 15, 15, 0, 0),
+              dependencies: [{to: 'HW'}, {to: 'SW / DNS/ Backups'}]}
+      ]},
+      {name: 'Hosting'},
+      {name: 'Setup', tasks: [
+          {id: 'HW', name: 'HW', color: '#F1C232', from: new Date(2013, 10, 18, 8, 0, 0), to: new Date(2013, 10, 18, 12, 0, 0)}
+      ]},
+      {name: 'Config', tasks: [
+          {id: 'SW / DNS/ Backups', name: 'SW / DNS/ Backups', color: '#F1C232', from: new Date(2013, 10, 18, 12, 0, 0), to: new Date(2013, 10, 21, 18, 0, 0)}
+      ]},
+      {name: 'Server', parent: 'Hosting', children: ['Setup', 'Config']},
+      {name: 'Deployment', parent: 'Hosting', tasks: [
+          {name: 'Depl. & Final testing', color: '#F1C232', from: new Date(2013, 10, 21, 8, 0, 0), to: new Date(2013, 10, 22, 12, 0, 0), 'classes': 'gantt-task-deployment'}
+      ]},
+      {name: 'Workshop', tasks: [
+          {name: 'On-side education', color: '#F1C232', from: new Date(2013, 10, 24, 9, 0, 0), to: new Date(2013, 10, 25, 15, 0, 0)}
+      ]},
+      {name: 'Content', tasks: [
+          {name: 'Supervise content creation', color: '#F1C232', from: new Date(2013, 10, 26, 9, 0, 0), to: new Date(2013, 10, 29, 16, 0, 0)}
+      ]},
+      {name: 'Documentation', tasks: [
+          {name: 'Technical/User documentation', color: '#F1C232', from: new Date(2013, 10, 26, 8, 0, 0), to: new Date(2013, 10, 28, 18, 0, 0)}
+      ]}
+  ];
   
   $scope.registerApi = function(api) {
 	  
@@ -20,6 +99,7 @@ function ChatController($scope, atmosphereService){
           api.tasks.on.moveEnd($scope, addEventName('tasks.on.moveEnd', function (eventName, data) {
 
               socket.push(atmosphere.util.stringifyJSON($scope.data));
+              socket.push(atmosphere.util.stringifyJSON(data.model));
 
           }));
 
@@ -65,20 +145,29 @@ function ChatController($scope, atmosphereService){
    
   };
 
-  //For demonstration of how you can customize the fallbackTransport using the onTransportFailure function
   request.onTransportFailure = function(errorMsg, request){
     atmosphere.util.info(errorMsg);
    
   };
 
   request.onMessage = function(response){
-	 // alert("Message Got")
     var responseText = response.responseBody;
     try{
       var message = atmosphere.util.parseJSON(responseText);
       
-     // $scope.data = [];
-      $scope.data = message;
+     
+      if(message.name != undefined) {
+    	  notify({
+              message: message.name + " got Re-sceduled",
+              classes: "alert-info",
+              templateUrl: "",
+              position: "right",
+              duration: 9000
+          }); 
+      } else {
+    	  $scope.data = message;
+      }
+      
 
     }catch(e){
       console.error("Error parsing JSON: ", responseText);
@@ -88,7 +177,6 @@ function ChatController($scope, atmosphereService){
 
   request.onClose = function(response){
    
-   // socket.push(atmosphere.util.stringifyJSON({ author: $scope.model.name, message: 'disconnecting' }));
   };
 
   request.onError = function(response){
